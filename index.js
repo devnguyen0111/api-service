703fb667bfe1e418ad556c307609bd8a39bc931d
Nhứt Nguyên <kieuphuongcsgo@gmail.com>
2023-09-26 14:21:56 +0700

    uninstall package serverless-http

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const serverless = require("serverless-http");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static("public"));
-
-const servlerlessHandler = serverless(app);
 
 if (process.platform == "win32") {
   const port = 3000;
@@ -16,4 +13,4 @@ if (process.platform == "win32") {
   });
 }
 
-export default servlerlessHandler;
+export default app;
